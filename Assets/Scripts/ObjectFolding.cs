e44aa65cc7abe92d40886e1bebd2e5eda92cb314
Neugodov Egor <eneugodov@inbox.ru>
2021-08-19 12:57:09 +0300

    upd
    upd

--- Assets/Scripts/ObjectFolding.cs
+++ Assets/Scripts/ObjectFolding.cs
@@ -11,9 +11,12 @@ public class ObjectFolding : MonoBehaviour
     [SerializeField] private Transform _leftPivot;
     [SerializeField] private Transform _rightPivot;
     [SerializeField] private Transform _lowerPivot;
+    [SerializeField] private AnimationCurve _animationCurve;
+    [SerializeField] private float _time;
 
     private GameObject[] _allDrawingParts;
-    private float _counter;
+    private Stack<Step> _steps = new Stack<Step>(10);
+    private bool isFolding;
 
     private void Awake()
     {
@@ -55,15 +58,29 @@ public class ObjectFolding : MonoBehaviour
 
     private void FoldingUpperObjects()
     {
+        
+        var step = new Step {Pivot = _upperPivot};
+
         foreach (var drawingPart in _allDrawingParts)
         {
             if (drawingPart.transform.position.z > _upperPivot.position.z)
             {
+                step.PreviousPivot = drawingPart.transform.parent;
                 drawingPart.transform.parent = _upperPivot;
             }
         }
         
-        _upperPivot.DORotate(new Vector3(-180, 0, 0), 1f);
+        var angleRotation = new Vector3(-180, 0, 0);
+        _upperPivot.DORotate(angleRotation, 1f);
+
+        //ReverseFoldingObjects(step,_upperPivot,angleRotation);
+    }
+
+    private void ReverseFoldingObjects(Step step, Transform pivot,Vector3 angle)
+    {
+        if (pivot != step.Pivot) return;
+        //pivot.DORotate(Vector3.zero, 1f);
+        
     }
     
     private void FoldingLowerObjects()
@@ -85,10 +102,11 @@ public class ObjectFolding : MonoBehaviour
         {
             if (drawingPart.transform.position.x > _rightPivot.position.x)
             {
-                drawingPart.transform.parent = _rightPivot;
+                drawingPart.transform.SetParent(_rightPivot);
             }
         }
-        _rightPivot.DORotate(new Vector3(0, 0, 180), 1f);
+        //_rightPivot.DORotate(new Vector3(0, 0, 180), 1f);
+        StartCoroutine(RotatePivot(_rightPivot, 1));
     }
     private void FoldingLeftObjects()
     {
@@ -102,4 +120,28 @@ public class ObjectFolding : MonoBehaviour
 
         _leftPivot.DORotate(new Vector3(0, 0, -180), 1f);
     }
+    
+    private IEnumerator RotatePivot(Transform pivot, int offset) 
+    {
+        var elapsed = 0f;
+        var eulers = transform.eulerAngles;
+        
+        while (elapsed < 1f)
+        {
+            elapsed += Time.fixedDeltaTime / _time;
+            Quaternion rot = Quaternion.Euler(eulers.x, eulers.y,
+                eulers.z + (_animationCurve.Evaluate(elapsed) * offset));
+
+            pivot.rotation = Quaternion.Lerp(pivot.rotation, rot, elapsed);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+}
+
+public struct Step
+{
+    public Transform Pivot;
+    public Transform[] Parts;
+    public Transform PreviousPivot;
 }
